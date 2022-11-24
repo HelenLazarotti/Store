62764ce6f6aa911aa03892cc17d5310345312d4f
Helen Lazarotti <74218577+HelenLazarotti@users.noreply.github.com>
2022-11-23 21:30:09 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/layout/Header';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Header>
+      <Home/>
+    </Header>
+    
+  ) 
+
+}
+
+export default App;
